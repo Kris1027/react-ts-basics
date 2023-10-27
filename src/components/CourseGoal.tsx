543548fa5de922a27893ba0a05fa65b3ba33fa2d
Kris1027 <obarzanek.work@gmail.com>
2023-10-27 13:52:19 +0200

    build CourseGoals component in two ways

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,37 @@
-export default function CourseGoal() {
+import { type PropsWithChildren } from 'react';
+
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        {children}
       </div>
       <button>DELETE</button>
     </article>
   );
 }
+
+//----------------------------------------------
+// import { type FC, type ReactNode } from 'react';
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>DELETE</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
